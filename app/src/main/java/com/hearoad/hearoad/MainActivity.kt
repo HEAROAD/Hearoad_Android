263ab4a2a0c82b3bacfa62f023ba460f4ae2a30a
chaeli <s.kcl5243@gmail.com>
2024-08-18 03:12:20 +0900

    FEAT: 초기세팅

---
+++ app/src/main/java/com/hearoad/hearoad/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hearoad.hearoad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
