cb25efe178287a053fd198d00bc040bffaf78bd3
chaeli <s.kcl5243@gmail.com>
2024-08-31 17:17:43 +0900

    TEST: chatting, camera

---
+++ app/src/main/java/com/hearoad/hearoad/ui/activity/ChatcameraActivity.kt
@@ -0,0 +1,41 @@
+package com.hearoad.hearoad.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.mediapipe.tasks.vision.core.RunningMode
+import com.hearoad.hearoad.databinding.ActivityChatcameraBinding
+import com.hearoad.hearoad.ui.viewmodel.HandLandmarkerHelper
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+class ChatcameraActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "Hand Landmarker"
+    }
+
+    private lateinit var binding: ActivityChatcameraBinding
+//    private lateinit var handLandmarkerHelper: HandLandmarkerHelper
+//    private var preview: Preview? = null
+//    private var imageAnalyzer: ImageAnalysis? = null
+//    private var camera: Camera? = null
+//    private var cameraProvider: ProcessCameraProvider? = null
+//    private var cameraFacing = CameraSelector.LENS_FACING_FRONT
+//
+//    private lateinit var backgroundExecutor: ExecutorService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChatcameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+}
--- app/src/main/java/com/hearoad/hearoad/ui/fragment/GuideFragment.kt
+++ app/src/main/java/com/hearoad/hearoad/ui/fragment/GuideFragment.kt
@@ -1,26 +1,36 @@
 package com.hearoad.hearoad.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hearoad.hearoad.R
+import com.hearoad.hearoad.databinding.FragmentGuideBinding
+import com.hearoad.hearoad.ui.activity.ChatroomActivity
+import com.hearoad.hearoad.ui.activity.MyvoiceActivity
 
 
 class GuideFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding : FragmentGuideBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_guide, container, false)
+        _binding = FragmentGuideBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.clGuide1.setOnClickListener {
+            val intent = Intent(requireContext(), ChatroomActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
